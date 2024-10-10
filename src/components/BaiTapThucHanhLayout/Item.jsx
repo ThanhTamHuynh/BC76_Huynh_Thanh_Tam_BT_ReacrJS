b5330937ba7119e3d4bb7facbc5c0c0050a7d7ed
HuynhThanhTam <huynhtam9x1998@gmail.com.vn>
2024-10-11 02:10:38 +0700

    sua code img

--- src/components/BaiTapThucHanhLayout/Item.jsx
+++ src/components/BaiTapThucHanhLayout/Item.jsx
@@ -6,7 +6,7 @@ const Item = () => {
   return (
     <section className="py-1">
       <div className=" bg-[#FFFFFF] text-center">
-        <img className="w-ful" src="./public/img1.jpg" alt="" />
+        <img className="w-ful" src="./img1.jpg" alt="" />
         <h2 className="font-bold my-3">Card title</h2>
         <p className="mb-5">
           Lorem ipsum dolor sit amet,
